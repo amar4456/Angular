b15c34c435ac8ff2eb94983d54bc8446d655dc9c
AmarKr2023 <am_kr@rocketmail.com>
2023-03-18 12:19:43 +0530

    Add -> employee Component & add it to home page

--- CRUD_Using_Local_Storage/src/app/pages/pages.module.ts
+++ CRUD_Using_Local_Storage/src/app/pages/pages.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { InlineEditingOneComponent } from './inline-editing-one/inline-editing-one.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 
 @NgModule({
   declarations: [
-    InlineEditingOneComponent
+    InlineEditingOneComponent,
+    EmployeeComponent
   ],
   imports: [
     CommonModule,
     PagesRoutingModule
   ],
-  exports:[InlineEditingOneComponent]
+  exports:[InlineEditingOneComponent,EmployeeComponent]
 })
 export class PagesModule { }
