c945b22adf0e73c28729de6975e42ae220d78571
AmarKr2023 <am_kr@rocketmail.com>
2023-03-31 10:31:27 +0530

    Compleated routing for Popolation Chart

--- CRUD_Using_Local_Storage/src/app/app-routing.module.ts
+++ CRUD_Using_Local_Storage/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEditComponent } from './pages/student/add-edit/add-edit.component';
 import { StudentRegCRUDLocalstorageComponent } from './pages/student-reg-crud-localstorage/student-reg-crud-localstorage.component';
+import { PopolationComponent } from './pages/amcharts/popolation/popolation.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'SL',
     component: StudentRegCRUDLocalstorageComponent,
+  },
+  {
+    path: 'popolation',
+    component: PopolationComponent,
   },
 ];
 
--- CRUD_Using_Local_Storage/src/app/app.component.ts
+++ CRUD_Using_Local_Storage/src/app/app.component.ts
@@ -34,6 +34,10 @@ export class AppComponent {
           {
             label: 'API',
             routerLink: ['/AE'],
+          },
+          {
+            label: 'Popolation Chart',
+            routerLink: ['/popolation'],
           }
         ]
       },
