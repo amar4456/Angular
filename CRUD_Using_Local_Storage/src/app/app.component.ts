3eec23a822d0decd47bfac3c5bcdcc66d71617f2
AmarKr2023 <am_kr@rocketmail.com>
2023-04-03 17:29:22 +0530

    Correction of Sidebar, Panelmenu auto expand.

--- CRUD_Using_Local_Storage/src/app/app.component.ts
+++ CRUD_Using_Local_Storage/src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { InlineEditingOneComponent } from './pages/inline-editing-one/inline-editing-one.component';
 import { PrimeNGConfig } from 'primeng/api';
 import { MenuItem } from 'primeng/api';
+import { PanelMenuStateService } from './services/panel-menu-state.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-root',
@@ -13,18 +19,31 @@ export class AppComponent {
   title = 'CRUD_LS';
 
   items: MenuItem[] = [];
+  isSidebarOpen: boolean = true;
 
   @ViewChild(InlineEditingOneComponent) inlineEditingOneComponent: InlineEditingOneComponent = new InlineEditingOneComponent;
 
   visibleSidebar: boolean=true;
 
-  constructor(private primengConfig: PrimeNGConfig) { }
+  constructor(private primengConfig: PrimeNGConfig,private panelMenuStateService: PanelMenuStateService) { }
 
   menuButton(){
-    this.visibleSidebar = !this.visibleSidebar
+    if (!this.isSidebarOpen) {
+      this.panelMenuStateService.cachePanelMenuState(this.items);
+      this.visibleSidebar = true
+    } else {
+      this.panelMenuStateService.cachePanelMenuState(this.items);
+      this.visibleSidebar = false
+    }
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 
   ngOnInit() {
+    const panelMenuState = this.panelMenuStateService.getCachedPanelMenuState();
+      if (panelMenuState) {
+        this.items = panelMenuState;
+      }
+
     this.primengConfig.ripple = true;
 
     this.items = [
