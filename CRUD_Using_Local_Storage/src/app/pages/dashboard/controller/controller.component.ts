a11817fa718125ac2253dff326d64c708f6215a0
AmarKr2023 <am_kr@rocketmail.com>
2023-04-04 13:48:27 +0530

    Adding Column with Rotated Label Chart_Color Issue

--- CRUD_Using_Local_Storage/src/app/pages/dashboard/controller/controller.component.ts
+++ CRUD_Using_Local_Storage/src/app/pages/dashboard/controller/controller.component.ts
@@ -312,6 +312,143 @@ export class ControllerComponent implements OnInit {
 
     //  Donut Chart End
 
+    //  Column with Rotated Labels Start
+
+    this.browserOnly(() => {
+      /* Chart code */
+      // Create root element
+      // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+      let root = am5.Root.new("columnChart");
+
+
+      // Set themes
+      // https://www.amcharts.com/docs/v5/concepts/themes/
+      root.setThemes([
+        am5themes_Animated.new(root)
+      ]);
+
+
+      // Create chart
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/
+      let chart = root.container.children.push(am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: "panX",
+        wheelY: "zoomX",
+        pinchZoomX: true
+      }));
+
+      // Add cursor
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+      let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+      cursor.lineY.set("visible", false);
+
+
+      // Create axes
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+      let xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
+      xRenderer.labels.template.setAll({
+        rotation: -90,
+        centerY: am5.p50,
+        centerX: am5.p100,
+        paddingRight: 15
+      });
+
+      xRenderer.grid.template.setAll({
+        location: 1
+      })
+
+      let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+        maxDeviation: 0.3,
+        categoryField: "country",
+        renderer: xRenderer,
+        tooltip: am5.Tooltip.new(root, {})
+      }));
+
+      let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        maxDeviation: 0.3,
+        renderer: am5xy.AxisRendererY.new(root, {
+          strokeOpacity: 0.1
+        })
+      }));
+
+
+      // Create series
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+      let series = chart.series.push(am5xy.ColumnSeries.new(root, {
+        name: "Series 1",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value",
+        sequencedInterpolation: true,
+        categoryXField: "country",
+        tooltip: am5.Tooltip.new(root, {
+          labelText: "{valueY}"
+        })
+      }));
+
+      // Color Issue
+
+      // series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
+      // series.columns.template.adapters.add("fill", function (fill, target) {
+      //   return chart.get("colors").getIndex(series.columns.indexOf(target));
+      // });
+
+      // series.columns.template.adapters.add("stroke", function (stroke, target) {
+      //   return chart.get("colors").getIndex(series.columns.indexOf(target));
+      // });
+
+      series.set("fill", am5.color(0xff0000,)); 
+
+
+      // Set data
+      let data = [{
+        country: "USA",
+        value: 2025
+      }, {
+        country: "China",
+        value: 1882
+      }, {
+        country: "Japan",
+        value: 1809
+      }, {
+        country: "Germany",
+        value: 1322
+      }, {
+        country: "UK",
+        value: 1122
+      }, {
+        country: "France",
+        value: 1114
+      }, {
+        country: "India",
+        value: 984
+      }, {
+        country: "Spain",
+        value: 711
+      }, {
+        country: "Netherlands",
+        value: 665
+      }, {
+        country: "South Korea",
+        value: 443
+      }, {
+        country: "Canada",
+        value: 441
+      }];
+
+      xAxis.data.setAll(data);
+      series.data.setAll(data);
+
+
+      // Make stuff animate on load
+      // https://www.amcharts.com/docs/v5/concepts/animations/
+      series.appear(1000);
+      chart.appear(1000, 100);
+    });
+
+    //  Column with Rotated Labels End
+
   }
 
 }
