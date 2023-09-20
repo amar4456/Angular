2e9b68c944c8c51f01db3698b1b5cc736bdeb780
AmarKr2023 <am_kr@rocketmail.com>
2023-09-20 11:00:06 +0530

    Image -> Upload - View - Download

--- CRUD_Using_Local_Storage/src/app/pages/image/image-edit/image-edit.component.ts
+++ CRUD_Using_Local_Storage/src/app/pages/image/image-edit/image-edit.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageEditComponent implements OnInit {
 
+  selectedImage: any | ArrayBuffer | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFileChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      const file = inputElement.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        this.selectedImage = e.target?.result;
+        console.log(this.selectedImage); // Add this line to check the value
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  downloadImage(): void {
+    if (this.selectedImage) {
+      const a = document.createElement('a');
+      a.href = this.selectedImage as string;
+      a.download = 'selected_image.png'; // You can set the filename here
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
   }
 
 }
