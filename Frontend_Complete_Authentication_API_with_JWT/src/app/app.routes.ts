604f1bcda7b3a87d37b7a8a421a5bb20897aa784
amar4456 <am_kr@rocketmail.com>
2023-12-15 19:14:45 +0530

    Create Component & Complete Routing for Login & Forgot

--- Frontend_Complete_Authentication_API_with_JWT/src/app/app.routes.ts
+++ Frontend_Complete_Authentication_API_with_JWT/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/user/login/login.component';
+import { ForgotComponent } from './features/user/forgot/forgot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'forgot', component: ForgotComponent },
+];
