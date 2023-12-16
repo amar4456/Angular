8a37a845586f7ad55466696aa24028d9fb3664d2
amar4456 <am_kr@rocketmail.com>
2023-12-16 16:08:22 +0530

    ADD -> No_Standalone_Frontend_Complete_Authentication_API_with_JWT Project Because of Module Issue

---
+++ No_Standalone_Frontend_Complete_Authentication_API_with_JWT/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'No_Standalone_Frontend_Complete_Authentication_API_with_JWT';
+}
