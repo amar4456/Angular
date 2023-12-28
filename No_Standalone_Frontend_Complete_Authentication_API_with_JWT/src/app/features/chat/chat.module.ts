461127085facefb9c2d06aa8476f3cafe6e3868c
amar4456 <am_kr@rocketmail.com>
2023-12-28 13:20:33 +0530

    Implement socket.io-client for Chat

--- No_Standalone_Frontend_Complete_Authentication_API_with_JWT/src/app/features/chat/chat.module.ts
+++ No_Standalone_Frontend_Complete_Authentication_API_with_JWT/src/app/features/chat/chat.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChattingComponent } from './chatting/chatting.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { ChattingComponent } from './chatting/chatting.component';
   ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ChatModule { }
--- No_Standalone_Frontend_Complete_Authentication_API_with_JWT/src/app/features/chat/chatting/chatting.component.ts
+++ No_Standalone_Frontend_Complete_Authentication_API_with_JWT/src/app/features/chat/chatting/chatting.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-chatting',
   templateUrl: './chatting.component.html',
-  styleUrl: './chatting.component.scss'
+  styleUrls: ['./chatting.component.scss']
 })
 export class ChattingComponent {
+  socket = io('http://localhost:8000');
+  message: string = '';
+  messages: { username: string; content: string; timestamp: string }[] = [];
+  username: any;
 
+  constructor() {
+    this.socket.on('chat message', (data: { username: string; content: string; timestamp: string }) => {
+      this.messages.push(data);
+    });
+  }
+
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      const userData: any = localStorage.getItem('userDetails');
+      const jsonObject = JSON.parse(userData);
+      this.username = jsonObject.name;
+    }
+  }
+
+  sendMessage() {
+    if (this.message.trim() !== '') {
+      const timestamp = new Date().toLocaleString();
+      const messageData = { username: this.username, content: this.message, timestamp: timestamp };
+      this.socket.emit('chat message', messageData);
+      this.message = '';
+    }
+  }
 }
